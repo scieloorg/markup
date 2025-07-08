0c9bd891c2813609e446d983b906ddb6f0df7248
Edgar <eduranm@dgb.unam.mx>
2025-07-08 14:22:59 -0600

    configuración de api reference

--- config/api_router.py
+++ config/api_router.py
@@ -1,25 +1,23 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-#from article.api.v1.views import ArticleViewSet
-#from issue.api.v1.views import IssueViewSet
-#from pid_provider.api.v1.views import PidProviderViewSet, FixPidV2ViewSet
-#from journal.api.v1.views import JournalViewSet, GenericJournalViewSet
+from reference.api.v1.views import ReferenceViewSet
 
-
-app_name = "pid_provider"
+app_name = "reference"
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-#router.register("article", ArticleViewSet, basename="Article")
-#router.register("issue", IssueViewSet, basename="Issue")
-#router.register("pid_provider", PidProviderViewSet, basename="pid_provider")
-#router.register("fix_pid_v2", FixPidV2ViewSet, basename="fix_pid_v2")
-#router.register("journal", JournalViewSet, basename="journal")
-
-
+router.register("reference", ReferenceViewSet, basename="reference")
 
 urlpatterns = router.urls
+
+
+#app_name = "pid_provider"
+
+#if settings.DEBUG:
+#    router = DefaultRouter()
+#else:
+#    router = SimpleRouter()
